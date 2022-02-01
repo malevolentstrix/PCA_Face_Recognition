99a05d5bca6e0ddd63111da116ee6508b6d2dfed
Lakshmi Warrier <lakshmi0105warrier@gmail.com>
2022-02-01 17:57:18 +0530

    2dpca done

--- src/2dpca.py
+++ src/2dpca.py
@@ -1,97 +1,101 @@
-from itertools import count
+from utils import get_faces, get_person_num, show_sample_faces, split_train_test
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-
-from utils import get_faces, show_sample_faces
-
-
-def find_mean_mat(facematrix):
-    #eqn 5
-    #print(len(facematrix), len(facematrix[0]), len(facematrix[0][0]))
-    sum_matr = np.empty((80,70), dtype=np.int8)
-    for img in facematrix:
-        sum_matr = np.add(img, sum_matr)
-    #print (sum_matr/len(facematrix))
-    return sum_matr/len(facematrix)
 
 faces = get_faces(zipfile_path="./Grp13Dataset.zip")
-#show_sample_faces(faces=faces)
+# show_sample_faces(faces=faces)
 
 faceshape = list(faces.values())[0].shape
 print("Face image shape:", faceshape)
+img_height, img_width = faceshape
 
 classes = set(filename.split("/")[0] for filename in faces.keys())
 print("Number of classes:", len(classes))
 print("Number of images:", len(faces))
 
-facematrix = []
-facelabel = []
-for key, val in faces.items():
-    if key.startswith("Grp13Person40/"):
-        continue
-    if key == "Grp13Person39/390_39.jpg":
-        continue
-    
-    #facematrix[i] is i-th image
-    facematrix.append(val)
-    facelabel.append(key.split("/")[0])
-
-facematrix = np.array(facematrix)
-
-M = find_mean_mat(facematrix)
-
-A=[]
-for img in facematrix:
-    #eqn 6
-    A.append(img-M)
-A= np.array(A)
-print(A.shape)
-
-C=[]
-for i in A:
-    #print(i.shape)
-    C.append(i.transpose() @ i)
-
-C = np.array(C)
-C = np.divide(C, len(facematrix))
-#eqn 7
-print(C)
-
-eigen_values, eigen_vectors = np.linalg.eig(C)
+training_set, testing_set = split_train_test(zipfilepath="./Grp13Dataset.zip")
 
 
-n_components = 50
-eigenfaces = eigen_vectors[:n_components]
+def get_matrix(training_list, img_height, img_width):
+
+    img_mat = np.zeros(
+        (len(training_list), img_height, img_width),
+        dtype=np.uint8)
+
+    i = 0
+    for img in training_list:
+        mat = np.asmatrix(training_list[img])
+        img_mat[i, :, :] = mat
+        i += 1
+    print("Matrix Size:", img_mat.shape)
+    return img_mat
+
+# def show_eigen_face(mean_subtracted, eig_no, new_bases):
+#     ev = new_bases[:, eig_no:eig_no + 1]
+#     print(new_bases.shape)
+#     print((mean_subtracted[0]@new_bases).shape)
+#     print(ev.shape)
+#     cv2.imshow("Eigen Face " + str(eig_no),  cv2.resize(np.array((80,50), dtype = np.uint8),(200, 200)))
+#     cv2.waitKey()
 
 
-fig, axes = plt.subplots(4, 4, sharex=True, sharey=True, figsize=(8, 10))
-for i in range(16):
-    axes[i % 4][i//4].imshow(eigenfaces[i].reshape(faceshape), cmap="gray")
-print("Showing the eigenfaces")
-plt.show()
+facematrix = get_matrix(training_set, img_height, img_width)
+no_of_images = facematrix.shape[0]
 
-#eqn 4
-weights = eigenfaces @ (facematrix - pca.mean_).T
-print("Shape of the weight matrix:", weights.shape)
+# eqn 5
+mean_face = np.mean(facematrix, 0)
+# eqn 6
+mean_subtracted = facematrix - mean_face
 
 
-def get_best_match(filename):
-    query = faces[filename].reshape(1, -1)
-    #eqn 22
-    query_weight = eigenfaces @ (query - pca.mean_).T
-    euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
-    best_match = np.argmin(euclidean_distance)
-    print("Best match %s with Euclidean distance %f" %
-          (facelabel[best_match], euclidean_distance[best_match]))
+mat_width = facematrix.shape[2]
+g_t = np.zeros((mat_width, mat_width))  # mxm
 
-    fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(8, 6))
-    axes[0].imshow(query.reshape(faceshape), cmap="gray")
-    axes[0].set_title("Query")
-    axes[1].imshow(facematrix[best_match].reshape(faceshape), cmap="gray")
-    axes[1].set_title("Best match")
-    plt.show()
+for i in range(no_of_images):
+
+    # multiplying net subtracted image with its transpose and adding in gt
+    temp = np.dot(mean_subtracted[i].T, mean_subtracted[i])
+    g_t += temp
+
+# dividing by total number of images
+g_t /= no_of_images
+# gt is the cov matrix - eq 7
+
+# finding eigen values and eigen vectors
+eig_val, eig_vec = np.linalg.eig(g_t)
+
+n = 20
+eigfaces = eig_vec[:, 0:n]
+
+# finding new coordinates using dot product new bases
+
+weight_matrix = np.dot(facematrix, eigfaces)
 
 
-get_best_match(filename="Grp13Person39/390_39.jpg")
-get_best_match(filename="Grp13Person40/391_40.jpg")
+def get_best_match(img):
+    img_mat = testing_set[img]
+    distances = []
+    for i in range(no_of_images):
+        temp_imgs = weight_matrix[i]
+        dist = np.linalg.norm(img_mat@eigfaces - temp_imgs)
+        distances += [dist]
+
+    min = np.argmin(distances)
+    return(min//8 + 1)
+
+
+correct_pred = 0
+wrong_pred = 0
+for img in testing_set:
+    person_num, img_num = get_person_num(filename=img)
+
+    best_match = get_best_match(img)
+    if person_num == best_match:
+        correct_pred += 1
+    else:
+        wrong_pred += 1
+total_pred = correct_pred+wrong_pred
+
+print(f" Correct preds: {correct_pred}/{total_pred}")
+print(f" Wrong preds: {wrong_pred}/{total_pred}")
+
+print(f"Accuracy {correct_pred*100/total_pred}%")
