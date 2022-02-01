7875a5dabfa470bc9b77451d5e12fbc3c667433d
MalevolentStrix <jithintvm1@gmail.com>
2022-02-01 22:33:55 +0530

    Replace the video feature with manual image reading

--- src/kpca/video.py
+++ src/kpca/video.py
@@ -13,35 +13,23 @@ TEXT_THICKNESS = 2
 
 def recognize_faces(mean_face, eigen_faces, classifier):
 	faceCascade = cv2.CascadeClassifier(HAAR_CASCADE_FRONTAL_FACE_PATH)
-	video_capture = cv2.VideoCapture(0)
 	
-	while(True):
-		# Capture frame
-	    ret, frame = video_capture.read()
-	    # Turn to gray scal
-	    grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	    # Detect faces
-	    faces = faceCascade.detectMultiScale(grayscaled_frame, minSize=(image.HORIZONTAL_SIZE, image.VERTICAL_SIZE))
-	    # Draw a rectangle around detected faces
-	    for face in faces:
-	        x, y, width, height = fd.resizeFace(face)
-	        cv2.rectangle(frame, (x, y), (x + width, y + height), FRAME_COLOR)
-	        captured_image = fd.cropImage(frame, fd.resizeFace(face))
-	        captured_image = fd.resizeImg(captured_image)
-	        captured_image = captured_image.convert('L')	# 'L' stands for grayscale mode
-	        captured_image = np.array(captured_image).ravel()
-	        captured_image = (np.array(captured_image) / image.NORMALIZE_FACTOR) - mean_face
-	        captured_image = np.dot(np.array(captured_image), eigen_faces.transpose())
-	        name = classifier.predict([captured_image])
-	        cv2.putText(frame, name[0], (x, y - TEXT_ELEVATION), fontFace=TEXT_FONT, fontScale=1, color=TEXT_COLOR, thickness=TEXT_THICKNESS)
-
-	    # Display the complete frame
-	    cv2.imshow('Video', frame)
-
-	    # Magic
-	    if cv2.waitKey(1) & 0xFF == ord(' ') and frame is not None and len(faces) > 0:
-	        captured_image = fd.cropImage(frame, fd.resizeFace(faces[0]))
-	        captured_image = fd.resizeImg(captured_image)
-
-	video_capture.release()
-	cv2.destroyAllWindows()
+	frame = cv2.imread('./caprio.jpg')
+	# Turn to gray scal
+	grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+	faces = faceCascade.detectMultiScale(grayscaled_frame, minSize=(image.HORIZONTAL_SIZE, image.VERTICAL_SIZE))
+	# Draw a rectangle around detected faces
+	for face in faces:
+		x, y, width, height = fd.resizeFace(face)
+		cv2.rectangle(frame, (x, y), (x + width, y + height), FRAME_COLOR)
+		captured_image = fd.cropImage(frame, fd.resizeFace(face))
+		captured_image = fd.resizeImg(captured_image)
+		captured_image = captured_image.convert('L')	# 'L' stands for grayscale mode
+		captured_image = np.array(captured_image).ravel()
+		captured_image = (np.array(captured_image) / image.NORMALIZE_FACTOR) - mean_face
+		captured_image = np.dot(np.array(captured_image), eigen_faces.transpose())
+		name = classifier.predict([captured_image])
+		cv2.putText(frame, name[0], (x, y - TEXT_ELEVATION), fontFace=TEXT_FONT, fontScale=1, color=TEXT_COLOR, thickness=TEXT_THICKNESS)
+		print("Detected person :", end='')
+		print(name[0])
