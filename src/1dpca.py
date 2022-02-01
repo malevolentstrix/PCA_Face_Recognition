8ccb9f64f5ecd44ef5a4b5c4a35467486cf1bf8a
lakshmi-warrier <lakshmi0105warrier@gmail.com>
2022-02-01 09:32:47 +0530

    testing done - prints accuracy

--- src/1dpca.py
+++ src/1dpca.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-from utils import get_faces, show_sample_faces
+from utils import get_faces, get_person_num, show_sample_faces, split_train_test
 
 
 faces = get_faces(zipfile_path="./Grp13Dataset.zip")
@@ -17,13 +17,11 @@ print("Number of images:", len(faces))
 
 facematrix = []
 facelabel = []
-for key, val in faces.items():
-    if key.startswith("Grp13Person40/"):
-        continue
-    if key == "Grp13Person39/390_39.jpg":
-        continue
-    
-    #fig 1
+
+training_set, testing_set = split_train_test(zipfilepath="./Grp13Dataset.zip")
+
+for key, val in training_set.items():
+    # fig 1
     facematrix.append(val.flatten())
     facelabel.append(key.split("/")[0])
 
@@ -31,38 +29,55 @@ facematrix = np.array(facematrix)
 
 pca = PCA().fit(facematrix)
 
-n_components = 50
+n_components = 40
 eigenfaces = pca.components_[:n_components]
 
 
 fig, axes = plt.subplots(4, 4, sharex=True, sharey=True, figsize=(8, 10))
 for i in range(16):
     axes[i % 4][i//4].imshow(eigenfaces[i].reshape(faceshape), cmap="gray")
-print(faceshape)
 print("Showing the eigenfaces")
 plt.show()
 
-#eqn 4
+# eqn 4
 weights = eigenfaces @ (facematrix - pca.mean_).T
 print("Shape of the weight matrix:", weights.shape)
 
 
 def get_best_match(filename):
     query = faces[filename].reshape(1, -1)
-    #eqn 22
+    # eqn 22
     query_weight = eigenfaces @ (query - pca.mean_).T
     euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
     best_match = np.argmin(euclidean_distance)
-    print("Best match %s with Euclidean distance %f" %
-          (facelabel[best_match], euclidean_distance[best_match]))
+    #print("Best match %s with Euclidean distance %f" %  (facelabel[best_match], euclidean_distance[best_match]))
 
     fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(8, 6))
+    person_num, img_num = get_person_num(filename=filename)
+
     axes[0].imshow(query.reshape(faceshape), cmap="gray")
-    axes[0].set_title("Query")
+    axes[0].set_title("Query - Person " + str(person_num))
     axes[1].imshow(facematrix[best_match].reshape(faceshape), cmap="gray")
-    axes[1].set_title("Best match")
-    plt.show()
+    axes[1].set_title("Best match - Person " + str((best_match//8) + 1))
+    # plt.show()
+    # uncomment the above line to see individual results of testing faces
+
+    return (((best_match//8) + 1), person_num)
 
 
-get_best_match(filename="Grp13Person39/390_39.jpg")
-get_best_match(filename="Grp13Person40/391_40.jpg")
+correct_pred = 0
+wrong_pred = 0
+total_pred = 0
+for key, val in testing_set.items():
+
+    predicted, actual = get_best_match(filename=key)
+    total_pred += 1
+    if predicted == actual:
+        correct_pred += 1
+    else:
+        wrong_pred += 1
+
+print(f" Correct preds: {correct_pred}/{total_pred}")
+print(f" Wrong preds: {wrong_pred}/{total_pred}")
+
+print(f"Accuracy {correct_pred*100/total_pred}%")
--- src/utils.py
+++ src/utils.py
@@ -24,3 +24,35 @@ def show_sample_faces(faces):
         axes[i % 4][i//4].imshow(faceimages[i], cmap="gray")
     print("Showing sample faces")
     plt.show()
+
+
+def get_person_num(filename):
+    filename = filename.replace("Grp13Person", "").split("/")
+    person_num = int(filename[0])
+    img_num = int(filename[1].split("_")[0]) % 10
+    return (person_num, img_num)
+
+
+def split_train_test(zipfilepath):
+    training_set = {}
+    testing_set = {}
+    with zipfile.ZipFile(zipfilepath) as facezip:
+        for filename in facezip.namelist():
+            if not filename.endswith(".jpg"):
+                continue
+
+            person_num, img_num = get_person_num(filename=filename)
+
+            # every 0th and 1st pic per person is sent to training set
+            if img_num == 0 or img_num == 1:
+                with facezip.open(filename) as image:
+
+                    testing_set[filename] = cv2.imdecode(np.frombuffer(
+                        image.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+            else:
+
+                with facezip.open(filename) as image:
+                    training_set[filename] = cv2.imdecode(np.frombuffer(
+                        image.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+
+    return training_set, testing_set
